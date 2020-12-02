f9710ddf7804135d8a1fac48c7dd451ef9b9948f
Dro1de1 <35335212+Dro1de1@users.noreply.github.com>
2020-12-02 22:16:18 +0100

    "finished day02"

--- day02/validate.js
+++ day02/validate.js
@@ -1,7 +1,31 @@
 const fs = require("fs")
-const file = fs.readFileSync("passwords.txt", "utf8")
-console.log(file)
-file.forEach(pw => console.log("jhal",pw))
+//make the date usefull
+let file = fs.readFileSync("passwords.txt", "utf8").trim().split("\n").map(pw => pw.split(" ")).map(line => {
+    return {
+        min : line[0].split("-")[0]-0,
+        max : line[0].split("-")[1]-0,
+        letter : line[1].split(":")[0],
+        pw : line[2]
+    }
+})
+
+//part1
+let masterCount = 0
+file.forEach(pw => {
+    let count = 0
+    for (i=0,length=pw.pw.length;i<length;i++) {
+        if (pw.pw.charAt(i) === pw.letter) count++
+    }
+    if ((pw.min<=count)&&(count<=pw.max)) masterCount++
+})
+console.log(masterCount)
 
 
-//console.log(file)
+//part2
+let masterCount1 = 0;
+file.forEach(pw => {
+    let count = 0
+    if ((pw.pw.charAt(pw.min-1)===pw.letter)&&(pw.pw.charAt(pw.max-1)===pw.letter)) {}
+    else if ((pw.pw.charAt(pw.min-1)===pw.letter)||(pw.pw.charAt(pw.max-1)===pw.letter)) masterCount1++
+})
+console.log(masterCount1)
